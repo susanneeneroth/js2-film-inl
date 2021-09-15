dd7249fc6608d9bbe1ec7ca32bf04be3c307ec6b
susanneeneroth <susanne@centimedia.se>
2021-09-15 16:10:49 +0200

    Test of bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './css/App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <div className='App'>Dev branch created</div>;
+  return (
+    <div className='App'>
+      <h1>Dev branch created</h1>
+      <Button variant='danger'>Bootstrap</Button>
+    </div>
+  );
 }
 
 export default App;
