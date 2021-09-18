ede51d09bfde34b2b2359250de899e1df53af6ca
susanneeneroth <susanne@centimedia.se>
2021-09-18 14:01:54 +0200

    Work on the api wrapper service

--- src/services/TMDBApi.js
+++ src/services/TMDBApi.js
@@ -1,8 +1,35 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const api_key = import.meta.env.TMDB_API_KEY;
+const api_key = `?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
 
-const getUpcoming = api.get('movie/upcoming', {
-  params: { api_key },
-});
+const get = async (endpoint) => {
+  const response = await axios.get(endpoint);
+
+  return response.data;
+};
+
+/**
+ * Get top rated movies
+ *
+ * @param {number} page Page of films to get
+ * @returns Promise
+ */
+export const getTopRated = async (page = null) => {
+  return get(`/movie/top_rated?page=${page}`);
+};
+
+/**
+ * Get popular movies
+ *
+ * @param {number} page Page of films to get
+ * @returns Promise
+ */
+
+export const getPopular = async (page = null) => {
+  return get(`/movie/popular${api_key}?page=${page}`);
+};
+
+export default {
+  getTopRated,
+};
